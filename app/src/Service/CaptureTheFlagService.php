0b6671c6cc26603d48de15c37c7a1100bc5e8e45
JPechberty <j.pechberty@gmail.com>
2023-12-12 01:37:21 +0100

    Update CaptureTheFlagService.php

--- app/src/Service/CaptureTheFlagService.php
+++ app/src/Service/CaptureTheFlagService.php
@@ -2,40 +2,33 @@
 
 namespace App\Service;
 
-use App\Entity\Operation;
-use App\Entity\User;
 use App\Repository\UserRepository;
 use Psr\Log\LoggerInterface;
 
 class CaptureTheFlagService
 {
 
+    const FLAG_API_PAGE_FOUND = "CTF_API_PAGE_FOUND";
+    const FLAG_TRANSFER_PAGE_FOUND = "CTF_TRANSFER_PAGE_FOUND";
+    const FLAG_FIRST_TRANSFER = "CTF_FIRST_TRANSFER";
+    const FLAG_AMOUNT_OBJECTIVE_COMPLETE = "CTF_AMOUNT_OBJECTIVE_COMPLETE";
 
     public function __construct(
-        private readonly string $rootDir,
-        private readonly LoggerInterface $ctfLogger,
-        private readonly UserRepository $userRepository,
+        private string          $rootDir,
+        private LoggerInterface $ctfLogger,
+        private UserRepository  $userRepository,
     )
     {
     }
 
-    public function logArray(Array $log): void
+    public function logArray(array $log): void
     {
         $this->save($log);
-
-        if($log['type'] === "OPERATION"){
-            $this->triggerLevel2($log);
-        }
-
+        $this->triggerCTFs($log);
     }
 
-
-    private function save(array $log): void
+    public function getFlags(): array
     {
-        $this->ctfLogger->info(json_encode($log));
-    }
-
-    public function getFlags(){
         $raw = file_get_contents($this->rootDir."/".$_ENV["CTF_LOG_FILE_PATH"]);
         $flags = [];
         foreach(explode("\n",$raw) as $line){
@@ -53,9 +46,63 @@ class CaptureTheFlagService
         });
     }
 
+
+    private function save(array $log): void
+    {
+        $this->ctfLogger->info(json_encode($log));
+    }
+
+
     // CTF CHECKING
 
-    private function triggerLevel2($log): void
+    private function isFlagAlreadyExist(string $flag): bool
+    {
+        $flags = $this->getFlags();
+         return count(array_filter($flags,function(array $item) use ($flag){
+             return $item['flag'] === $flag;
+         })) === 1;
+
+    }
+
+    private function triggerCTFs(array $log){
+        if($log['type'] === "OPERATION"){
+            $this->triggerFirstTransferCTF($log);
+        }
+
+        if($log['type'] === "ACCESS"){
+            $this->triggerTransferPageFoundCTF($log);
+            $this->triggerApiPageFoundCTF($log);
+        }
+    }
+
+    private function triggerTransferPageFoundCTF(array $log): void
+    {
+        if( $log['type'] === 'ACCESS' &&
+            $log['path'] === '/admin/transfer' &&
+            !$this->isFlagAlreadyExist(self::FLAG_TRANSFER_PAGE_FOUND)
+        ){
+            $this->save([
+                "date"=>(new \DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')))->format('d/m/Y H:i:s'),
+                "flag"=>self::FLAG_TRANSFER_PAGE_FOUND
+            ]);
+        }
+
+    }
+
+    private function triggerApiPageFoundCTF(array $log){
+        if( $log['type'] === 'ACCESS' &&
+            $log['path'] === '/api/extract' &&
+            !$this->isFlagAlreadyExist(self::FLAG_API_PAGE_FOUND)
+        ){
+            $this->save([
+                "date"=>(new \DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')))->format('d/m/Y H:i:s'),
+                "flag"=>self::FLAG_API_PAGE_FOUND
+            ]);
+        }
+    }
+
+
+    private function triggerFirstTransferCTF($log): void
     {
         $shadowAccountNumber = $this->userRepository
             ->findOneBy(["email"=>$_ENV["SHADOW_ACCOUNT_MAIL"]])
@@ -66,7 +113,7 @@ class CaptureTheFlagService
         ) {
             $this->save([
                 "date"=>(new \DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')))->format('d/m/Y H:i:s'),
-                "flag"=>"CTF_2_FIRST_TRANSFER"
+                "flag"=>self::FLAG_FIRST_TRANSFER
             ]);
         }
     }
